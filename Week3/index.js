75f01ce7e3bbe064747cbe7bb076c65254c993e8
oyolah <oyolahmustapha2015@gmail.com>
2019-09-03 02:49:12 +0300

    working

--- Week3/index.js
+++ Week3/index.js
@@ -1,38 +1,48 @@
 let myString = "hello,this,is,a,difficult,to,read,sentence";
 console.log(myString.length)
-console.log("----------------------------------------")
+
+
 var newString = myString.replace(/,/g, " ");
 console.log(newString)
-console.log("----------------------------------------")
+
+
 let favoriteAnimals = ['blowfish', 'capricorn', 'giraffe'];
-console.log("----------------------------------------")
+
+
 favoriteAnimals.push('turtle');
 console.log(favoriteAnimals)
-console.log("----------------------------------------")
+
+
 let arr = favoriteAnimals.splice(1, 0, "meerket");
-console.log("----------------------------------------")
+
+
 console.log("My array new value will be 5 since i have add new element in the array")
-console.log("----------------------------------------")
+
+
 console.log(favoriteAnimals)
 console.log('The array has a length of: ' + favoriteAnimals.length)
-console.log("----------------------------------------")
+
+
 let arr2 = favoriteAnimals.splice(3, 1,);
 console.log(favoriteAnimals)
-console.log("----------------------------------------")
+
+
 var index = favoriteAnimals.indexOf("meerket")
 console.log('The item you are looking for is at index: '+ index)
 
-console.log("----------------------------------------")
+
 function adder(a, b, c){
   return(a+b+c);
 }
 adder(3, 4, 5)
-console.log("----------------------------------------")
+
+
 function colorCar(color) { 
   console.log(color);
 }
 colorCar('a red car');
-console.log("----------------------------------------")
+
+
 Person = {
   id: 1,
   name: "Eliza",
@@ -49,6 +59,34 @@ if (3 === 3) {
 }
 console.log()
 
+// Q12 // empty object
+let object = {
+
+}
+
+// Q13-14
+let teachers = {
+  Zohir: 'Htmlcss',
+  Mohamed: 'JavaScript',
+  John: 'Javascript'
+}
+console.log(teachers)
+
+// Q 15
+let x = [1,2,3];
+    let y = [1,2,3];
+    let z = y;
+   x == y,
+
+    console.log(x == y) // false
+    console.log(x === y) // false
+  console.log(z == y)  // true
+  console.log(z == x)  //false
+
+// Q 17
+  let bar = 42;
+typeof typeof bar; // it returns 'string'.
+     
 
 
 
